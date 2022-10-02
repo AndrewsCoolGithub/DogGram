deebb6caae9c07b4d7ce8a78db6dc2005d3ffd77
Andrew <admin@thebouncer.app>
2022-10-02 05:27:56 -0500

    Init

---
+++ DogGram/Views/Screens/CommentsView.swift
@@ -0,0 +1,149 @@
+//
+//  CommentsView.swift
+//  DogGram
+//
+//  Created by Andrew Kestler on 10/7/20.
+//
+
+import SwiftUI
+
+struct CommentsView: View {
+    
+    @Environment(\.colorScheme) var colorScheme
+    @State var submissionText: String = ""
+    @State var commentArray = [CommentModel]()
+    
+    var post: PostModel
+    
+    @State var profilePicture: UIImage = UIImage(named: "logo.loading")!
+    
+    @AppStorage(CurrentUserDefaults.userID) var currentUserID: String?
+    @AppStorage(CurrentUserDefaults.displayName) var currentUserDisplayName: String?
+    
+    var body: some View {
+        VStack {
+            
+            // Messages scrollview
+            ScrollView {
+                LazyVStack {
+                    ForEach(commentArray, id: \.self) { comment in
+                        MessageView(comment: comment)
+                    }
+                }
+            }
+            
+            // Bottom HStack
+            HStack {
+                
+                Image(uiImage: profilePicture)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40, alignment: .center)
+                    .cornerRadius(20)
+                
+                TextField("Add a comment here...", text: $submissionText)
+                
+                Button(action: {
+                    if textIsAppropriate() {
+                        addComment()
+                    }
+                }, label: {
+                    Image(systemName: "paperplane.fill")
+                        .font(.title2)
+                })
+                .accentColor(colorScheme == .light ? Color.MyTheme.purpleColor : Color.MyTheme.yellowColor)
+            }
+            .padding(.all, 6)
+            
+        }
+        .padding(.horizontal)
+        .navigationBarTitle("Comments")
+        .navigationBarTitleDisplayMode(.inline)
+        .onAppear(perform: {
+            getComments()
+            getProfilePicture()
+        })
+    }
+    
+    // MARK: FUNCTIONS
+    
+    func getProfilePicture() {
+        
+        guard let userID = currentUserID else { return }
+        
+        ImageManager.instance.downloadProfileImage(userID: userID) { (returnedImage) in
+            if let image = returnedImage {
+                self.profilePicture = image
+            }
+        }
+    }
+    
+    func getComments() {
+        
+        guard self.commentArray.isEmpty else { return }
+        print("GET COMMENTS FROM DATABASE")
+        
+        if let caption = post.caption, caption.count > 1 {
+            let captionComment = CommentModel(commentID: "", userID: post.userID, username: post.username, content: caption, dateCreated: post.dateCreated)
+            self.commentArray.append(captionComment)
+        }
+
+        DataService.instance.downloadComments(postID: post.postID) { (returnedComments) in
+            self.commentArray.append(contentsOf: returnedComments)
+        }
+    }
+    
+    func textIsAppropriate() -> Bool {
+        // Check if the text has curses
+        // Check if the text is long enough
+        // Check if the text is blank
+        // Check for innapropriate things
+        
+        
+        // Checking for bad words
+        let badWordArray: [String] = ["shit", "ass"]
+
+        let words = submissionText.components(separatedBy: " ")
+
+        for word in words {
+            if badWordArray.contains(word) {
+                return false
+            }
+        }
+        
+        // Checking for minimum character count
+        if submissionText.count < 3 {
+            return false
+        }
+        
+        return true
+    }
+    
+    func addComment() {
+        
+        guard let userID = currentUserID, let displayName = currentUserDisplayName else { return }
+        
+        DataService.instance.uploadComment(postID: post.postID, content: submissionText, displayName: displayName, userID: userID) { (success, returnedCommentID) in
+            if success, let commentID = returnedCommentID {
+                let newComment = CommentModel(commentID: commentID, userID: userID, username: displayName, content: submissionText, dateCreated: Date())
+                self.commentArray.append(newComment)
+                self.submissionText = ""
+                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+            }
+        }
+        
+    }
+    
+}
+
+struct CommentsView_Previews: PreviewProvider {
+    
+    static let post = PostModel(postID: "asdf", userID: "asdf", username: "asdf", dateCreated: Date(), likeCount: 0, likedByUser: false)
+    
+    static var previews: some View {
+        NavigationView {
+            CommentsView(post: post)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
---
+++ DogGram/Views/Screens/FeedView.swift
@@ -0,0 +1,34 @@
+//
+//  FeedView.swift
+//  DogGram
+//
+//  Created by Andrew Kestler on 10/7/20.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    
+    @ObservedObject var posts: PostArrayObject
+    var title: String
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false, content: {
+            LazyVStack {
+                ForEach(posts.dataArray, id: \.self) { post in
+                    PostView(post: post, showHeaderAndFooter: true, addHeartAnimationToView: true)
+                }
+            }
+        })
+        .navigationBarTitle(title)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            FeedView(posts: PostArrayObject(shuffled: false), title: "Feed Test")
+        }
+    }
+}
